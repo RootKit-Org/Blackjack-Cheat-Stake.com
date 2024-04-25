3bef98538c9407a2ef651f45a15e6f0ccae1ce9a
Elijah Harmon <elijahharmon@gmail.com>
2024-04-24 23:55:56 -0700

    updated simulation to be more correct

--- simulator/blackjack.py
+++ simulator/blackjack.py
@@ -1,33 +1,61 @@
 import random
 
 class Blackjack:
-    def __init__(self):
-        self.decks = 8
-        self.cutCardLocation = 4
-        self.cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-        self.players = 3
+    def __init__(
+            self,
+            decks=8,
+            cutCardLocation=4,
+            cards=[2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11],
+            players=3,
+            playerBank=1000,
+            baseBet=5,
+            spreads=[
+                [2, 2],
+                [3, 4],
+                [5, 8],
+            ],
+            spreadCutOff=-2,
+            blackjackPayout=2.5
+    ):
+        self.decks = decks
+        self.cutCardLocation = cutCardLocation
+        self.cards = cards
+        self.players = players
         self.deck = []
 
-        self.playerBank = 1000
+        self.playerBank = playerBank
+        self.baseBet = baseBet
+        self.spreads = sorted(spreads, reverse=True)
+        self.spreadCutOff = spreadCutOff
+        self.blackjackPayout = blackjackPayout
 
         self.runningCount = 0
         
     def dealCard(self):
+        """Deal a card from the deck and count it."""
+
         card = self.deck.pop()
         self.countCard(card)
         return card
 
     def countCard(self, card):
+        """Count the card and update the running count."""
+
         if card in [2, 3, 4, 5, 6]:
             self.runningCount += 1
         elif card in [10, 11]:
             self.runningCount -= 1
 
     def shuffleDeck(self):
+        """Shuffle the deck and reset the running count."""
+
+        # 4x is the base cards in a deck x 4
         self.deck = (self.cards * 4) * self.decks
         random.shuffle(self.deck)
 
     def calculateScore(hand: list):
+        """Calculate the score of a hand. If the hand is a soft hand, return the highest possible score."""
+
         score = sum(hand)
         if score > 21 and 11 in hand:
             hand[hand.index(11)] = 1
@@ -35,6 +63,7 @@ class Blackjack:
         return score
 
     def basicStrategy(self, player_hand, dealer_hand):
+        """Return the basic strategy for the player's hand."""
         player_score = Blackjack.calculateScore(player_hand)
         dealer_score = dealer_hand[0]
 
@@ -57,8 +86,6 @@ class Blackjack:
                 else:
                     return "hit"
             elif player_score == 11:
-                if dealer_score == 11:
-                    return "hit"
                 return "double"
             elif player_score == 10:
                 if dealer_score in [10, 11]:
@@ -78,6 +105,8 @@ class Blackjack:
         # Soft
         else:
             if player_score >= 19:
+                if dealer_score in [6]:
+                    return "double"
                 return "stand"
             elif player_score == 18:
                 if dealer_score in [3, 4, 5, 6]:
@@ -101,7 +130,8 @@ class Blackjack:
                 return "hit"
 
     def split(player_hand, dealer_hand):
-        
+        """Return whether the player should split their hand."""
+
         if player_hand[0] == player_hand[1]:
             if player_hand[0] == 11:
                 return True
@@ -116,17 +146,14 @@ class Blackjack:
                 return True
             elif player_hand[0] == 6 and dealer_hand[0] < 7:
                 return True
-            elif player_hand[0] == 4 and dealer_hand[0] < 7:
-                return True
+            # elif player_hand[0] == 4 and dealer_hand[0] < 7:
+            #     return True
             elif player_hand[0] in [2, 3] and dealer_hand[0] < 8:
                 return True
             
-
         return False
 
-    def simulate(self, playerWinning=0):
-        if playerWinning > 0:
-            self.playerBank = playerWinning
+    def simulate(self):
         dealerWinnings = 0
         self.runningCount = 0
 
@@ -135,8 +162,7 @@ class Blackjack:
         rounds = []
         wins = 0
         losses = 0
-        totalBet = 0
-
+        runningBet = 0
 
         while len(self.deck) > (self.cutCardLocation*52):
 
@@ -149,124 +175,141 @@ class Blackjack:
 
             trueCount = self.runningCount / (len(self.deck)/52)
 
-            if trueCount <= -2:
+            # TODO something is wrong with the count
+            # we currently make way more money when the count is negative
+            if trueCount <= self.spreadCutOff:
                 continue
 
-            baseBet = 5
-            if trueCount >= 5:
-                bet = baseBet * 5
-            elif trueCount >= 4:
-                bet = baseBet * 3
-            elif trueCount >= 3:
-                bet = baseBet * 3
-            if trueCount >= 2:
-                bet = baseBet * 2
-            elif trueCount >= 1:
-                bet = baseBet * 2
-            else:
-                bet = baseBet
-            # bet = baseBet
-
+            bet = self.baseBet
+            # Growing bet based off spread and true count
+            for item in self.spreads:
+                if trueCount >= item[0]:
+                    bet = item[1] * self.baseBet
+                    break
+            
+            # Placing bet and removing from bank
+            runningBet += bet
             self.playerBank -= bet
-            totalBet += bet
 
             roundLive = True
-            playerDouble = False
-
             currentHand = len(player_hands)
             handSplit = False
-            for i in range(currentHand):
-                if Blackjack.split(player_hands[i], dealer_hand):
-                    player_hands.append([player_hands[i].pop(), self.dealCard()])
-                    player_hands[i].append(self.dealCard())
-                    totalBet += bet
-                    self.playerBank -= bet
-                    handSplit = True
+
+            # Check if we have pair and should split
+            # We only split once, modify if you want infinite
+            if Blackjack.split(player_hands[0], dealer_hand):
+                player_hands.append([player_hands[0].pop(), self.dealCard()])
+                player_hands[0].append(self.dealCard())
+
+                # Pay bet for splitting
+                self.playerBank -= bet
+
+                # TODO letting us do inifite splits if we add it later on
+                handSplit = True
 
             if self.playerBank < 0:
                 return None
 
+            # Loop through all hands
             for player_hand in player_hands:
+                # 1. If dealer has an ace, offer insurance
+                # 2. Dealer check if they have BJ
+                # 3. If dealer has BJ, check if player has BJ, games ends instantly, push and/or payout insurance
+
+                # 4. If dealer didn't have BJ, player plays
+                # 5. If player has BJ, player wins 3:2
+                
+                # 6. Play can hit, stand, double, split
+
                 roundLive = True
-                while roundLive:
-                    player_score = Blackjack.calculateScore(player_hand)
-                    dealer_score = Blackjack.calculateScore(dealer_hand)
+                insurance = False
+                doubled = False
+
+                player_score = Blackjack.calculateScore(player_hand)
+                dealer_score = Blackjack.calculateScore(dealer_hand)
+
+                # TODO double check the player gets paid immediately
+                # when dealer also had BJ
+                if player_score == 21:
+                    self.playerBank += bet*self.blackjackPayout
+                    wins += 1
+
+                    roundLive = False
+
+                else:
+                    if dealer_hand[0] == 11:
+                        # Player is offered insurance
+                        if trueCount >= 3:
+                            self.playerBank -= bet / 2
+                            insurance = True
 
                     if dealer_score == 21:
-                        if trueCount >= 3:
-                            self.playerBank += bet * .5
-                        else:
-                            dealerWinnings += bet
+                        if insurance:
+                            self.playerBank += bet
+
                         losses += 1
                         roundLive = False
 
-                    if player_score == 21:
-                        # print("Blackjack! You win!")
-                        self.playerBank += 2.5 * bet
+
+                # Playing thru the round with 1 hand
+                while roundLive:
+                    action = self.basicStrategy(player_hand, dealer_hand)
+
+                    # Stake doesn't allow double after split
+                    if handSplit and action == "double":
+                        action = "hit"
+
+                    if action == "hit":
+                        player_hand.append(self.dealCard())
+                        continue
+
+                    elif action == "double":
+                        player_hand.append(self.dealCard())
+
+                        doubled = True
+                        self.playerBank -= bet
+
+                    player_score = Blackjack.calculateScore(player_hand)
+
+                    if player_score > 21:
+                        losses += 1
+                        roundLive = False
+                        break
+
+                    # if we get here, player is done with getting cards
+                    # dealer plays out their hand
+                    while dealer_score < 17:
+                        dealer_hand.append(self.dealCard())
+                        dealer_score = Blackjack.calculateScore(dealer_hand)
+
+                    # Check who won
+                    gameConclusion = "push"
+                    if dealer_score > 21:
+                        gameConclusion = "win"
+                    elif dealer_score > player_score:
+                        gameConclusion = "loss"
+                    elif dealer_score < player_score:
+                        gameConclusion = "win"
+
+
+                    if gameConclusion == "win":
+                        if doubled:
+                            self.playerBank += bet*2
+
+                        self.playerBank += bet*2
                         wins += 1
-                        roundLive = False
-                    elif player_score > 21:
-                        # print("Bust! You lose!")
-                        dealerWinnings += bet
-                        losses += 1
-                        roundLive = False
+
+                    elif gameConclusion == "push":
+                        if doubled:
+                            self.playerBank += bet
+                        self.playerBank += bet
                     else:
+                        losses += 1
 
-                        action = self.basicStrategy(player_hand, dealer_hand)
+                    roundLive = False
+                    break
 
-                        if action == "hit":
-                            player_hand.append(self.dealCard())
-                            player_score = Blackjack.calculateScore(player_hand)
-
-                        else:
-                            if action == "double":
-                                player_hand.append(self.dealCard())
-                                player_score = Blackjack.calculateScore(player_hand)
-                                self.playerBank -= bet
-                                totalBet += bet
-                                if not handSplit:
-                                    self.playerBank -= bet
-                                    totalBet += bet
-                                else:
-                                    action = "hit"
-
-                                if player_score > 21:
-                                    # print("Bust! You lose!")
-                                    dealerWinnings += bet
-                                    losses += 1
-                                    roundLive = False
-
-                            while dealer_score < 17:
-                                dealer_hand.append(self.dealCard())
-                                dealer_score = Blackjack.calculateScore(dealer_hand)
-                                # print(f"Dealer's hand: {dealer_hand}")
-
-                            if dealer_score > 21:
-                                # print("Dealer busts! You win!")
-                                self.playerBank += bet * 2
-                                wins += 1
-                                roundLive = False
-                            elif dealer_score > player_score:
-                                # print("Dealer wins!")
-                                dealerWinnings += bet
-                                losses += 1
-                                roundLive = False
-                            elif dealer_score < player_score:
-                                # print("You win!")
-                                if action == "double":
-                                    self.playerBank += (bet*2) * 2
-                                else:
-                                    self.playerBank += bet * 2
-                                wins += 1
-                                break
-                            else:
-                                # print("It's a tie!")
-                                if action == "double":
-                                    self.playerBank += bet * 2
-                                else:
-                                    self.playerBank += bet
-                                roundLive = False
 
                 rounds.append(self.playerBank)
 
-        return self.playerBank, totalBet, rounds, wins, losses
+        return self.playerBank, runningBet, rounds, wins, losses
--- simulator/main.py
+++ simulator/main.py
@@ -1,89 +1,100 @@
 from blackjack import Blackjack
 import matplotlib.pyplot as plt
 
-# Your existing code
-gamesToSimulate = 100
-startBank = 10000
-games = []
-totalBankRuns = 0
-totalHourly = 0
-totalNegHourly = 0
-totalPosHourly = 0
-totalRounds = 0
-totalEndBank = 0
+def main():
+    # Your existing code
+    gamesToSimulate = 1
+    startBank = 10000
+    games = []
+    totalBankRuns = 0
+    totalHourly = 0
+    totalNegHourly = 0
+    totalPosHourly = 0
+    totalRounds = 0
+    totalEndBank = 0
 
-for game in range(gamesToSimulate):
-    player, dealer = startBank, 0
-    totalBet = 0
-    rounds = []
-    wins, losses = 0, 0
-    bankRun = False
-    for _ in range(100):
-        blackjack = Blackjack()
-        result = blackjack.simulate(player)
-        if result is None:
-            bankRun = True
-            break
-        player = result[0]
-        totalBet += result[1]
-        rounds.extend(result[2])
-        wins += result[3]
-        losses += result[4]
+    for game in range(gamesToSimulate):
+        player, dealer = startBank, 0
+        totalBet = 0
+        rounds = []
+        wins, losses = 0, 0
+        bankRun = False
+        for _ in range(10000):
+            blackjack = Blackjack(
+                decks=8,
+                cutCardLocation=4,
+                baseBet=10,
+                spreadCutOff=-2,
+                playerBank=player
+            )
+            result = blackjack.simulate()
+            if result is None:
+                bankRun = True
+                break
+            player = result[0]
+            totalBet += result[1]
+            rounds.extend(result[2])
+            wins += result[3]
+            losses += result[4]
 
-    games.append({
-        "startBank": startBank,
-        "endBank": player,
-        "totalBet": totalBet,
-        "rounds": rounds,
-        "wins": wins,
-        "losses": losses,
-        "bankRun": bankRun
-    })
-    if bankRun:
-        totalBankRuns += 1
+        games.append({
+            "startBank": startBank,
+            "endBank": player,
+            "totalBet": totalBet,
+            "rounds": rounds,
+            "wins": wins,
+            "losses": losses,
+            "bankRun": bankRun
+        })
+        if bankRun:
+            totalBankRuns += 1
 
-for idx, game in enumerate(games):
-    startBank = game["startBank"]
-    player = game["endBank"]
-    totalBet = game["totalBet"]
-    rounds = game["rounds"]
-    wins = game["wins"]
-    losses = game["losses"]
-    bankRun = game["bankRun"]
+    for idx, game in enumerate(games):
+        startBank = game["startBank"]
+        player = game["endBank"]
+        totalBet = game["totalBet"]
+        rounds = game["rounds"]
+        wins = game["wins"]
+        losses = game["losses"]
+        bankRun = game["bankRun"]
 
-    totalHourly += (player-startBank)/round(len(rounds)/60)
-    if player-startBank < 0:
-        totalNegHourly += 1
-    else:
-        totalPosHourly += 1
+        totalHourly += (player-startBank)/round(len(rounds)/60)
+        if player-startBank < 0:
+            totalNegHourly += 1
+        else:
+            totalPosHourly += 1
 
-    totalRounds += len(rounds)
-    totalEndBank += player
+        totalRounds += len(rounds)
+        totalEndBank += player
 
-    # print(f"\n\nGame {idx+1}:")
-    # print(f"Starting Bank: {startBank}")
-    # print(f"End Bank: {player}")
-    # print(f"Total Bet: {totalBet}")
-    # print(f"Wins: {wins}")
-    # print(f"Losses: {losses}")
-    print(f"Winning percentage: {wins/(wins+losses)}")
+        # print(f"\n\nGame {idx+1}:")
+        # print(f"Starting Bank: {startBank}")
+        # print(f"End Bank: {player}")
+        # print(f"Total Bet: {totalBet}")
+        # print(f"Wins: {wins}")
+        # print(f"Losses: {losses}")
+        print(f"Winning percentage: {wins/(wins+losses)}")
 
-    # print(f"Rounds: {len(rounds)}")
-    # print(f"Hours: {round(len(rounds)/60)}")
-    # print(f"$ per hour: {(player-startBank)/round(len(rounds)/60)}")
+        # print(f"Rounds: {len(rounds)}")
+        # print(f"Hours: {round(len(rounds)/60)}")
+        # print(f"$ per hour: {(player-startBank)/round(len(rounds)/60)}")
 
-print(f"\n\nTotal Games: {gamesToSimulate}")
-print(f"Starting Bank: ${startBank}")
-print(f"Average Rounds per game: {round(totalRounds/gamesToSimulate, 2)}")
-print(f"Average End Bank: ${round(totalEndBank/gamesToSimulate, 2)}")
-print(f"Bank Run Percentage: {round((totalBankRuns/gamesToSimulate)*100, 2)}%")
-print(f"Average Hourly: ${round(totalHourly/gamesToSimulate, 2)}")
-print(f"Positive Hourly Percentage: {round((totalPosHourly/gamesToSimulate)*100, 2)}%")
-print(f"Negative Hourly Percentage: {round((totalNegHourly/gamesToSimulate)*100, 2)}%")
+    print(f"\n\nTotal Games: {gamesToSimulate}")
+    print(f"Starting Bank: ${startBank}")
+    print(f"Average Rounds per game: {round(totalRounds/gamesToSimulate, 2)}")
+    print(f"Average End Bank: ${round(totalEndBank/gamesToSimulate, 2)}")
+    print(f"Bank Run Percentage: {round((totalBankRuns/gamesToSimulate)*100, 2)}%")
+    print(f"Average Hourly: ${round(totalHourly/gamesToSimulate, 2)}")
+    print(f"Positive Hourly Percentage: {round((totalPosHourly/gamesToSimulate)*100, 2)}%")
+    print(f"Negative Hourly Percentage: {round((totalNegHourly/gamesToSimulate)*100, 2)}%")
 
-# Plotting code
-plt.plot(rounds)
-plt.title('Blackjack Rounds')
-plt.xlabel('Round Number')
-plt.ylabel('Value')
-plt.show()
+    # Plotting code
+    plt.plot(rounds)
+    plt.title('Blackjack Rounds')
+    plt.xlabel('Round Number')
+    plt.ylabel('Value')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
